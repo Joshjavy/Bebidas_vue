0ecdb892c8c7bc9c09cc3ca4dc6878534c7be036
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-05-19 17:24:00 -0600

    253. Notificaciones en Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-  import { RouterView } from 'vue-router'
-  import Header from './components/Header.vue'
-  import Modal from './components/Modal.vue'
+    import { RouterView } from 'vue-router'
+    import Header from './components/Header.vue'
+    import Modal from './components/Modal.vue'
+    import Notificacion from './components/Notificacion.vue';
   </script>
 
 <template>
@@ -11,6 +12,6 @@
   <RouterView/>
  </main>
 <Modal/>
-
+<Notificacion/>
 </template>
 
